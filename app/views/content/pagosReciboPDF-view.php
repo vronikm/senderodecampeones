3af38e48c706fe81dc15c588fae71f417d278ce1
vronikm <veronicaquinde@digitech.com.ec>
2026-04-25 23:56:04 -0500

    Ajustar para envio de correo

--- app/views/content/pagosReciboPDF-view.php
+++ app/views/content/pagosReciboPDF-view.php
@@ -58,7 +58,7 @@
     // logo : 80 de largo por 55 de alto
     //,,ancho,
 
-    $pdf->Image(APP_URL.'app/views/imagenes/fotos/sedes/'.$sede['sede_foto'], 34, 10, 28, 28);
+    $pdf->Image(APP_URL.'app/views/imagenes/fotos/sedes/'.$sede['sede_foto'], 52, 10, 28, 28);
 
     $pdf->SetLineWidth(0.1); $pdf->Rect(10, 10, 190, 40, "D"); $x=15; $y=13;
   
--- app/views/content/pagospendienteReciboPDF-view.php
+++ app/views/content/pagospendienteReciboPDF-view.php
@@ -54,7 +54,7 @@
 
         // logo : 80 de largo por 55 de alto
         //,,ancho,
-        $pdf->Image(APP_URL.'app/views/imagenes/fotos/sedes/'.$sede['sede_foto'], 24, 10, 32, 32);
+        $pdf->Image(APP_URL.'app/views/imagenes/fotos/sedes/'.$sede['sede_foto'], 52, 10, 28, 28);
         $pdf->SetLineWidth(0.1); $pdf->Rect(10, 10, 190, 40, "D"); $x=15; $y=13;
 
         $pdf->SetXY( $x, $y ); $pdf->SetFont( "Arial", "B", 11 ); $pdf->Cell( 260, 8, "FORMATIVA SENDERO DE CAMPEONES", 0, 0, 'C'); $y+=5;
